b02d738a34128f5a6e920d0617545637434a841f
[yewonb] <satoo3@naver.com>
2025-07-14 11:15:17 +0900

    movies models

--- moviesite/movies/models.py
+++ moviesite/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Actors(models.Model):
   name=models.CharField(max_length=10)
@@ -16,5 +17,14 @@ class Movies(models.Model):
   rating=models.CharField(max_length=10)
   director_name=models.CharField(max_length=10)
   actors=models.ForeignKey(Actors, on_delete=models.CASCADE)
+
+User=get_user_model
+
+class Comments(models.Model):
+  username=models.ForeignKey(User, on_delete=models.CASCADE)
+  content=models.TextField()
+  create_date=models.DateTimeField(auto_now_add=True)
+
+
 
 
