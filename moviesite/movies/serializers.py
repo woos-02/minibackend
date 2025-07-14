f0e6350283774eeacbe1920a11685053ef1dd64e
[yewonb] <satoo3@naver.com>
2025-07-14 14:47:17 +0900

    serializers

--- moviesite/movies/serializers.py
+++ moviesite/movies/serializers.py
@@ -1,17 +1,21 @@
-from rest_framework import serializer
+from rest_framework import serializers
 from .models import *
 
-class ActorsSerializer(serializer.ModelSerializer):
+class ActorsSerializer(serializers.ModelSerializer):
   class Meta:
     model=Actors
     fields = ['name', 'character', 'image_url']
 
-class MoviesSerializer(serializer.ModelSerialzer):
+class MoviesSerializer(serializers.ModelSerializer):
+  actors = ActorsSerializer(many=True)
+  
   class Meta:
     model=Movies
     fields= '__all__' 
 
-class CommentsSerializer(serializer.ModelSerializer):
+class CommentsSerializer(serializers.ModelSerializer):
+  username=serializers.StringRelatedField(read_only=True)
+  
   class Meta:
     model=Comments
     fields=['username', 'content', 'create_date']
