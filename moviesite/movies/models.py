e04eb005b8ac312d2975b51104a6ef118e127f3b
[yewonb] <satoo3@naver.com>
2025-07-14 10:59:44 +0900

    movies models

--- moviesite/movies/models.py
+++ moviesite/movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Actors(models.Model):
+  name=models.CharField(max_length=10)
+  character=models.CharField(max_length=10)
+  image_url=models.TextField()
+
+class Movies(models.Model):
+  title_kor=models.CharField(max_length=200)
+  title_eng=models.CharField(max_length=200)
+  poster_url=models.TextField()
+  genre=models.CharField(max_length=100)
+  showtime=models.CharField(max_length=10)
+  release_date=models.CharField(max_length=10)
+  plot=models.TextField()
+  rating=models.CharField(max_length=10)
+  director_name=models.CharField(max_length=10)
+  actors=models.ForeignKey(Actors, on_delete=models.CASCADE)
+
+
